7064c82795fddb7309f99b20942655e2022ae468
Ian Kiku <iankiku@users.noreply.github.com>
2018-12-30 17:11:32 -0600

    update template

--- todo/views.py
+++ todo/views.py
@@ -14,7 +14,9 @@ Demonstrate simple CRUD Operations in django DB
 """
 "Things to do"
 # List Todo Items in DB
-
+def All_Items (request):
+        all_items = TodoItem.objects.all
+        return render(request, 'todo.html', {'all_items': all_items})
 # Add a Todo Item
 
 # Delete a TodoItem
